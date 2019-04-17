b7c6ba205a1af515c3575bfa9575af83911f6f3b
Marat Guysin <mguysin17@gmail.com>
2019-04-17 15:19:47 +0100

    update on loops, missing iterations on if statements

---
+++ Week 2 Whats your name age difference/venv/Adding/for_loops.py
@@ -0,0 +1,50 @@
+
+
+
+
+#  for place_holder in list_object
+#      #Execute this block of code
+#
+
+# ids_list=['fiat', 'Lambo', 'Russian Car', 'potato']
+#
+# counter=0
+#
+# for id in ids_list:
+#     print(counter +1, '-', id)
+#     counter+=1
+
+#Embeded for loops
+        #_________1__________2
+# embeded_data=[[1, 2 ,3], [7, 12, 1], ['Skoda Felicia Fun', 'Panda 4*4']]
+#
+#
+# for data_list in embeded_data:
+#     print(data_list)
+#     for data in data_list:
+#           print(data)
+
+
+
+#Loops for dictionaries
+
+# accounts={
+#     1:{"name": "Arya", "money": "$1000000"},
+#     2:{"name": "Bob Builder", "money": "$10000"},
+#     3:{"name": "Sponge", "money": "$50000"},
+#}
+
+# for values in accounts.values():
+#     print(values)
+#     print(type(values))
+
+# for acc_dictionary in accounts.values():
+#     print(acc_dictionary)
+#     for embeded_val in acc_dictionary.values():
+#         print (embeded_val)
+
+# for key in accounts:
+#     print(accounts[key])
+#     for embeded_key in accounts[key]:
+#         print(accounts[key][embeded_key])
+
